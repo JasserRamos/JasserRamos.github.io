89564580dc1866cf8cdeb9dc41ece4e81746e555
JasserRamos <49545442+JasserRamos@users.noreply.github.com>
2022-09-15 01:25:33 -0600

    Likes ya con cookies

---
+++ app.js
@@ -0,0 +1,39 @@
+function setLike(articleID) {
+  document.cookie = articleID + "=y";
+}
+
+function printCookies() {
+  alert(document.cookie);
+}
+function resetCookies() {
+  var allCookies = document.cookie.split(";");
+
+  // The "expire" attribute of every cookie is
+  // Set to "Thu, 01 Jan 1970 00:00:00 GMT"
+  for (var i = 0; i < allCookies.length; i++)
+    document.cookie = allCookies[i] + "=;expires=" + new Date(0).toUTCString();
+
+  displayCookies.innerHTML = document.cookie;
+}
+function hasLiked(articleID) {
+  var cookies = document.cookie.split(";");
+  var result = false;
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].split("=");
+    if (cookie[0].trim() == articleID) {
+      result = true;
+    }
+  }
+  return result;
+}
+var likecount = document.cookie.split(";").length;
+function awesomeClick(articleID) {
+  alert("alooo");
+  if (!hasLiked(articleID)) {
+    setLike(articleID);
+    likecount++;
+    alert("se dio like " + likecount + " vez");
+  } else {
+    alert("You cant like or dislike an article twice");
+  }
+}
