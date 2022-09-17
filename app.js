a063c7088a4b79ee5f90ed85d93743c95ea4c2f6
JasserRamos <49545442+JasserRamos@users.noreply.github.com>
2022-09-16 22:43:13 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,55 @@
+function cookieDate(d) {
+  function d2(n) {
+    return n < 10 ? "0" + n : n;
+  }
+  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+  return (
+    "" +
+    days[d.getUTCDay()] +
+    ", " +
+    d2(d.getUTCDate()) +
+    "-" +
+    months[d.getUTCMonth()] +
+    "-" +
+    d.getUTCFullYear() +
+    " " +
+    d2(d.getUTCHours()) +
+    ":" +
+    d2(d.getUTCMinutes()) +
+    ":" +
+    d2(d.getUTCSeconds()) +
+    " GMT"
+  );
+}
 function setLike(articleID) {
-  document.cookie = articleID + "=y; expires=Tue, 19 Jan 2038 03:14:07 UTC";
+  var CookieDate = new Date();
+  const daysToExpire = new Date(2147483647 * 1000).toGMTString();
+  document.cookie =
+    articleID +
+    "=like; expires= Fri, 1 Jan 2038" +
+    ";max-age=" +
+    500 * 24 * 60 * 60 +
+    ";path=/";
+  alert(document.cookie);
 }
 
 function printCookies() {
-  alert(document.cookie);
+  alert(document.cookie.split(";"));
 }
 function resetCookies() {
   var allCookies = document.cookie.split(";");
@@ -12,8 +58,6 @@ function resetCookies() {
   // Set to "Thu, 01 Jan 1970 00:00:00 GMT"
   for (var i = 0; i < allCookies.length; i++)
     document.cookie = allCookies[i] + "=;expires=" + new Date(0).toUTCString();
-
-  displayCookies.innerHTML = document.cookie;
 }
 function hasLiked(articleID) {
   var cookies = document.cookie.split(";");
@@ -36,6 +80,7 @@ function awesomeClick(articleID) {
     alert("No puedes darle like dos veces");
   }
 }
+/*
 getListItemsForView(
   _spPageContextInfo.webAbsoluteUrl,
   "BECAS",
@@ -93,4 +138,5 @@ function getListItems(webUrl, listTitle, queryText) {
       "content-type": "application/json; odata=verbose",
     },
   });
-}
+  
+}*/
