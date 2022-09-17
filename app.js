46466533714354fe48348d2d338c8d38b4606251
JasserRamos <49545442+JasserRamos@users.noreply.github.com>
2022-09-16 21:52:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function setLike(articleID) {
-  document.cookie = articleID + "=y; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+  document.cookie = articleID + "=y; expires=2147483647 ";
 }
 
 function printCookies() {
@@ -35,4 +35,62 @@ function awesomeClick(articleID) {
   } else {
     alert("No puedes darle like dos veces");
   }
+}
+getListItemsForView(
+  _spPageContextInfo.webAbsoluteUrl,
+  "BECAS",
+  "VistaAnioActual"
+)
+  .done(function (data) {
+    var itemsCount = data.d.results.length;
+    alert(itemsCount);
+  })
+  .fail(function (error) {
+    console.log(JSON.stringify(error));
+  });
+function getListItemsForView(webUrl, listTitle, viewTitle) {
+  var viewQueryUrl =
+    webUrl +
+    "/_api/web/lists/getByTitle('" +
+    listTitle +
+    "')/Views/getbytitle('" +
+    viewTitle +
+    "')/ViewQuery";
+  return getJson(viewQueryUrl).then(function (data) {
+    var viewQuery = data.d.ViewQuery;
+    return getListItems(webUrl, listTitle, viewQuery);
+  });
+}
+
+function getJson(url) {
+  return $.ajax({
+    url: url,
+    type: "GET",
+    contentType: "application/json;odata=verbose",
+    headers: {
+      Accept: "application/json;odata=verbose",
+    },
+  });
+}
+
+function getListItems(webUrl, listTitle, queryText) {
+  var viewXml = "<View><Query>" + queryText + "</Query></View>";
+  var url = webUrl + "/_api/web/lists/getbytitle('" + listTitle + "')/getitems";
+  var queryPayload = {
+    query: {
+      __metadata: { type: "SP.CamlQuery" },
+      ViewXml: viewXml,
+    },
+  };
+
+  return $.ajax({
+    url: url,
+    method: "POST",
+    data: JSON.stringify(queryPayload),
+    headers: {
+      "X-RequestDigest": $("#__REQUESTDIGEST").val(),
+      Accept: "application/json; odata=verbose",
+      "content-type": "application/json; odata=verbose",
+    },
+  });
 }
